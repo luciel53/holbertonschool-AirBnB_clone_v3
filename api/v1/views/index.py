449fd54795e64cef3d850d42b1aa9f52ba99a6b9
william0863 <granger.w35@gmail.com>
2022-12-06 16:22:38 +0100

    test task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,5 +8,5 @@ from flask import jsonify
 @app_views.route('/status', strict_slashes=False)
 def app_views_status():
     """returns status"""
-    return jsonify({"status": "OK"})
-
+    dictionary = {"status": "OK"}
+    return jsonify(dictionary)
